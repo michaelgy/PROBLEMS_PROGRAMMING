8b03050b7918b69a1ca96258b3c16f1dbe5c40a4
michaelgy <michael801898@gmail.com>
2017-10-09 00:43:40 -0500

    Add files via upload

---
+++ UVA/102.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    int b1[3],b2[3],b3[3];
+    char assign[3];
+    for(cin >> b1[0]; !cin.eof(); cin >> b1[0]){
+        cin >> b1[1] >> b1[2];
+        cin >> b2[0] >> b2[1] >> b2[2];
+        cin >> b3[0] >> b3[1] >> b3[2];
+
+        if(b3[2]>=b2[2]){
+            if(b3[2]>=b1[2]){
+                assign[2] = 'G';
+            }
+        }
+    }
+}
